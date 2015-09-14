abfae550c6c6d4a961f60895a8c43738e3ade4d1
megge <ria6@yandex.ru>
2015-09-14 10:07:46 +0400

    при экспорте рецепт добавляется, если его нет в системе, и обновляется, если есть

--- app/api/export_import.py
+++ app/api/export_import.py
@@ -30,53 +30,95 @@ def upload_file():
                     return key
 
         def recipe_init_func(row):
+            recipe = Recipe.query.filter_by(title_lang_ru=row['title_lang_ru']).first()
             chef = Chef.query.filter_by(email=row['chef']).first()
             set = Set.query.filter_by(title_lang_ru=row['set']).first()
             categories = Category.query.filter(Category.title_lang_ru. in_(row['categories'].split(','))).all()
             cuisine_types = CuisineType.query.filter(CuisineType.title_lang_ru. in_(row['cuisine_types'].split(','))).all()
             tools = Tool.query.filter(Tool.title_lang_ru. in_(row['tools'].split(','))).all()
             wines = Wine.query.filter(Wine.title_lang_ru. in_(row['wines'].split(','))).all()
-            recipe = Recipe(title_lang_ru=row['title_lang_ru'],
-                            title_lang_en=row['title_lang_en'],
-                            description_lang_ru=row['description_lang_ru'],
-                            description_lang_en=row['description_lang_en'],
-                            spicy=row['spicy'],
-                            complexity=row['complexity'],
-                            time=time_sec_to_min(row['time']),
-                            amount_of_persons=row['amount_of_persons'],
-                            video_lang_ru=row['video_lang_ru'],
-                            video_lang_en=row['video_lang_en'],
-                            type=get_type(row['type']),
-                            chef_id=chef.id,
-                            set_id=set.id,
-                            categories=categories,
-                            cuisine_types=cuisine_types,
-                            tools=tools,
-                            wines=wines
-                            )
+            if recipe:
+                recipe.title_lang_ru = row['title_lang_ru']
+                recipe.title_lang_en = row['title_lang_en']
+                recipe.description_lang_ru = row['description_lang_ru']
+                recipe.description_lang_en = row['description_lang_en']
+                recipe.spicy = row['spicy']
+                recipe.complexity = row['complexity']
+                recipe.time = time_sec_to_min(row['time'])
+                recipe.amount_of_persons = row['amount_of_persons']
+                recipe.video_lang_ru = row['video_lang_ru']
+                recipe.video_lang_en = row['video_lang_en']
+                recipe.type = get_type(row['type'])
+                recipe.chef_id = chef.id
+                recipe.set_id = set.id
+                recipe.categories = categories
+                recipe.cuisine_types = cuisine_types
+                recipe.tools = tools
+                recipe.wines = wines
+                db.session.commit()
+            else:
+                recipe = Recipe(title_lang_ru=row['title_lang_ru'],
+                                title_lang_en=row['title_lang_en'],
+                                description_lang_ru=row['description_lang_ru'],
+                                description_lang_en=row['description_lang_en'],
+                                spicy=row['spicy'],
+                                complexity=row['complexity'],
+                                time=time_sec_to_min(row['time']),
+                                amount_of_persons=row['amount_of_persons'],
+                                video_lang_ru=row['video_lang_ru'],
+                                video_lang_en=row['video_lang_en'],
+                                type=get_type(row['type']),
+                                chef_id=chef.id,
+                                set_id=set.id,
+                                categories=categories,
+                                cuisine_types=cuisine_types,
+                                tools=tools,
+                                wines=wines
+                                )
             return recipe
 
         def ingredient_init_func(row):
             recipe = Recipe.query.filter_by(title_lang_ru=row['recipe']).first()
-            ingredient = Ingredient(title_lang_ru=row['title_lang_ru'],
-                                    title_lang_en=row['title_lang_en'],
-                                    amount=row['amount'],
-                                    unit_lang_ru=row['unit_lang_ru'],
-                                    unit_lang_en=row['unit_lang_en'],
-                                    recipe_id=recipe.id
-                                    )
+            ingredient = Ingredient.query.filter_by(title_lang_ru=row['title_lang_ru'], recipe_id=recipe.id).first()
+            if ingredient:
+                ingredient.title_lang_ru = row['title_lang_ru']
+                ingredient.title_lang_en = row['title_lang_en']
+                ingredient.amount = row['amount']
+                ingredient.unit_lang_ru = row['unit_lang_ru']
+                ingredient.unit_lang_en = row['unit_lang_en']
+                ingredient.recipe_id = recipe.id
+                db.session.commit()
+            else:
+                ingredient = Ingredient(title_lang_ru=row['title_lang_ru'],
+                                        title_lang_en=row['title_lang_en'],
+                                        amount=row['amount'],
+                                        unit_lang_ru=row['unit_lang_ru'],
+                                        unit_lang_en=row['unit_lang_en'],
+                                        recipe_id=recipe.id
+                                        )
             return ingredient
 
         def instruction_init_func(row):
             recipe = Recipe.query.filter_by(title_lang_ru=row['recipe']).first()
-            instruction = InstructionItem(step_number=row['step_number'],
-                                          time=time_sec_to_min(row['time']),
-                                          video_lang_ru=row['video_lang_ru'],
-                                          video_lang_en=row['video_lang_en'],
-                                          description_lang_ru=row['description_lang_ru'],
-                                          description_lang_en=row['description_lang_en'],
-                                          recipe_id=recipe.id
-                                    )
+            instruction = InstructionItem.query.filter_by(step_number=row['step_number'], recipe_id=recipe.id).first()
+            if instruction:
+                instruction.step_number = row['step_number']
+                instruction.time = time_sec_to_min(row['time'])
+                instruction.video_lang_ru = row['video_lang_ru']
+                instruction.video_lang_en = row['video_lang_en']
+                instruction.description_lang_ru = row['description_lang_ru']
+                instruction.description_lang_en = row['description_lang_en']
+                instruction.recipe_id = recipe.id
+                db.session.commit()
+            else:
+                instruction = InstructionItem(step_number=row['step_number'],
+                                              time=time_sec_to_min(row['time']),
+                                              video_lang_ru=row['video_lang_ru'],
+                                              video_lang_en=row['video_lang_en'],
+                                              description_lang_ru=row['description_lang_ru'],
+                                              description_lang_en=row['description_lang_en'],
+                                              recipe_id=recipe.id
+                                              )
             return instruction
 
         request.save_book_to_database(field_name='file', session=db.session,
